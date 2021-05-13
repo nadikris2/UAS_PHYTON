fa6524700e8fe623b71165551105ce038e5bc7e3
Nadikris1 <nadikris2@gmail.com>
2021-05-13 14:42:55 +0700

    Update products.py

--- venv/models/products.py
+++ venv/models/products.py
@@ -1,7 +1,9 @@
-from . import db
+# pos/models/products.py
+from pos.models import db
 #menarik data didalam database mysql untuk dapat di gunakan secara global 
-class Contactus(db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    users = db.Column(db.String(100), unique=True)
-    hub = db.Column(db.String(100))
-    isi = db.Column(db.String(100))
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(45), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
